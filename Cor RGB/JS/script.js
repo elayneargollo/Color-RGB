05ffbffa35b44b9649f9663e0f76c422fab27700
Elayne Argollo <48841005+elayneargollo@users.noreply.github.com>
2020-05-27 17:46:57 -0300

    System Color RGB

---
+++ Cor RGB/JS/script.js
@@ -0,0 +1,32 @@
+const corvermelho = '#corvermelho';
+const corazul = '#corazul';
+const corverde = '#corverde';
+
+var data = '';
+var datvermelho = '';
+var datazul = '';
+var datverde = '0;'
+
+
+function trocarCor(vermelho, verde, azul){
+    var data = document.querySelector('.quadrado');
+    data.style.backgroundColor = 'rgb('+vermelho + ',' + verde + ',' +azul +')';
+}
+
+function updateCor(id){
+
+    data = document.querySelector(corvermelho);
+    datvermelho = document.querySelector('#textcorvermelho');
+    datvermelho.value = data.value;
+
+    data = document.querySelector(corazul);
+    datazul = document.querySelector('#textcorAzul');
+    datazul.value = data.value;
+
+    data = document.querySelector(corverde);
+    datverde = document.querySelector('#textcorVerde');
+    datverde.value = data.value;
+     
+    trocarCor(datvermelho.value,datverde.value,datazul.value);
+}
+
